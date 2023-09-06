3645a3cf20f805f142d1582b0dbbd2e82af7478c
neenu joy <neenujoy@neenus-MacBook-Air.local>
2023-09-06 22:19:13 +0530

    First Commit

---
+++ Assignment1.java
@@ -0,0 +1,10 @@
+public class Assignment1 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Name: Neenu Joy");
+        System.out.println("Address1: abcd");
+        System.out.println("Place: Thodupuzha");
+        System.out.println("State:Kerala");
+    }
+}
---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld
+{
+    public static void main(String[] args) {
+        // Print "Hello, World!" to the console
+        System.out.println("Neenu");
+        System.out.println("123ß");
+    }
+}
+
