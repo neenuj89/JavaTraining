e32e916a78dc268c28a1c32bb717227d9d81f2d7
neenu joy <neenujoy@neenus-MacBook-Air.local>
2023-09-07 19:37:07 +0530

    second commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,8 +2,8 @@ public class HelloWorld
 {
     public static void main(String[] args) {
         // Print "Hello, World!" to the console
-        System.out.println("Neenu");
-        System.out.println("123ß");
+        System.out.println("Hello");
+        System.out.println("world");
     }
 }
 
