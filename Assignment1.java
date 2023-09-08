b4d9f4b313744c909e1f69cbe18c3e1aa2ee1ec4
neenu joy <neenujoy@neenus-MacBook-Air.local>
2023-09-08 21:06:34 +0530

    assignment2

--- Assignment1.java
+++ Assignment1.java
@@ -1,10 +1,15 @@
 public class Assignment1 {
 
+    
     public static void main(String[] args) {
+    String name="Neenu";
+    String addresss1="Abc";
+    String place="Thodupuzha";
+    String state="Kerala";
 
-        System.out.println("Name: Neenu Joy");
-        System.out.println("Address1: abcd");
-        System.out.println("Place: Thodupuzha");
-        System.out.println("State:Kerala");
+        System.out.println(name);
+        System.out.println(addresss1);
+        System.out.println(place);
+        System.out.println(state);
     }
 }
---
+++ Assignment2_variables.java
@@ -0,0 +1,18 @@
+public class Assignment2_variables {
+    public static void main(String[] args) {
+        int num1=30;
+        char char1='a';
+        float float1=13.1f;
+        double db=1234567890.12345678901233444;
+        String s="string";
+        Boolean b=true;
+        System.out.println("Declared valirables");
+System.out.println("Int:"+num1);
+System.out.println("Char:"+char1);
+System.out.println("Float:"+float1);
+System.out.println("Double:"+db);
+System.out.println("String:"+s);
+System.out.println("Boolean:"+b);
+
+    }
+}
