d52412cb986d90b3f9e90b210e330f26abd89feb
neenu joy <neenujoy@neenus-MacBook-Air.local>
2023-09-11 22:17:51 +0530

    multiple object and class- commit

---
+++ School.java
@@ -0,0 +1,21 @@
+public class School {
+ public static void main(String[] args) {
+        Student student1=new Student();
+        student1.name="abc";
+        student1.age=22;
+        student1.regNum="abc123";
+        System.out.println("Student 1 details");
+        System.out.println("name: "+student1.name);
+System.out.println("age: "+student1.age);
+System.out.println("name: "+student1.regNum);
+Student student2=new Student();
+        student2.name="abc131";
+        student2.age=54;
+        student2.regNum="123445";
+        System.out.println("Student2 details");
+        System.out.println("name: "+student2.name);
+System.out.println("age: "+student2.age);
+System.out.println("name: "+student2.regNum);
+      
+    }
+}
---
+++ Student.java
@@ -0,0 +1,5 @@
+public class Student{
+    String name;
+    int age;
+    String regNum;
+}
