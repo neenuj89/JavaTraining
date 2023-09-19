82910b8634871a324b51fd07945647838432fd82
neenu joy <neenujoy@neenus-MacBook-Air.local>
2023-09-19 21:38:35 +0530

    commit

--- IfElse.java
+++ IfElse.java
@@ -1,3 +1,19 @@
+import java.util.Scanner;
+
 public class IfElse {
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num = 0;
+        System.out.println("enter the num");
+        num = scanner.nextInt();
+        if(num<18){
+            System.out.println("num is lesss than 18");
+        }
+        else if(num<25){
+            System.out.println("num less than 25");
+        }
+        else
+        System.out.println("num>25");
+        scanner.close();
+    }
 }
--- LoopExample.java
+++ LoopExample.java
@@ -1,3 +1,32 @@
+import java.util.Scanner;
+
 public class LoopExample {
-    
+    public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+        int num = 0, limit = 0, i = 0;
+        System.out.println("enter the num");
+        num = scanner.nextInt();
+        System.out.println("Enter the limit of multiplication table");
+        limit = scanner.nextInt();
+
+        System.out.println("Mutiplication table\n-----for loop--------\n");
+        for (i = 0; i < limit; i++) {
+            System.out.printf(" %d * %d =%d\n", num, i, num * i);
+        }
+        
+        System.out.println("--------while loop-----------------\n");
+        i = 0;
+        while (i < limit) {
+            System.out.printf(" %d * %d =%d\n", num, i, num * i);
+            i++;
+        }
+        
+        i = 0;
+        System.out.println("------------do-while loop------------\n");
+        do {
+            System.out.printf(" %d * %d =%d\n", num, i, num * i);
+            i++;
+        } while (i < limit);
+        scanner.close(); 
+    }
 }
