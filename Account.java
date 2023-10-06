91c5166d69fd9776d51827e5724bd591605069eb
neenu <neenuj89@gmail.com>
2023-10-06 22:35:47 +0530

    commit to new branch

--- Account.java
+++ Account.java
@@ -2,12 +2,27 @@ public class Account {
     String accountNum;
     String accountHolder;//instance variable. access via object.
     float bal=0;
-    static  String accountType="Current"; //static variable. Access via class.obj cannot access static variable
-    //static method. accessed via class
+    static  String accountType; //static variable. Access via class.obj cannot access static variable
+    
+ Account(){
+    accountHolder="name123";
+    accountType="Savings";
+}   
+
+Account(String aString,float af){
+    accountHolder=aString;
+    accountType="Current";
+    bal=bal+af;
+    
+}
+
+//static method. accessed via class
     public static void accountType(){
+        
         System.out.println(accountType);
     }
     
+
     public void deposit(float amount){
         bal=bal+amount;
     }
--- Bank.java
+++ Bank.java
@@ -1,11 +1,16 @@
 
 public class Bank{
     public static void main(String[] args) {
+        float balance=0;
         Account acc1 =new Account();
+        Account acc2= new Account("account2",100);
         acc1.deposit(100);
         acc1.balanceCheck();
         acc1.withdrawAmount(50);
+        acc1.accountType();
         Account.accountType();
+        acc2.balanceCheck();
+
 
     }
 }
